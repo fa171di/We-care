8cd0680b7f2b6cce37d27ece10b8607ca06740a6
fa171di <fa1791di@gmail.com>
2023-05-08 12:26:47 +0300

    1

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth:sanctum')->get('/rr', function (Request $request) {
-    return $request->user();
-});
+
