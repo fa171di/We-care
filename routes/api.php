2a3d457c999478485571a7ea5d83571b10adfa47
Nawar <nawwaralissa1997@gmail.com>
2023-05-11 03:35:12 -0700

    ghghghghgh

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:sanctum')->get('/kkk', function (Request $request) {
+    return $request->user();
+});
+
+
+
+
+
 
