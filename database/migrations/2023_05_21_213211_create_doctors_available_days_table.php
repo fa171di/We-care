1cc4c3fd8024465e99c7a7c6bc314be335c6f7e6
fa171di <fa1791di@gmail.com>
2023-05-22 00:45:28 +0300

    doctor

--- database/migrations/2023_05_21_213211_create_doctors_available_days_table.php
+++ database/migrations/2023_05_21_213211_create_doctors_available_days_table.php
@@ -8,10 +8,8 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('doctor_available_days', function (Blueprint $table) {
             $table->id();
@@ -29,11 +27,9 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('expert_available_days');
+        Schema::dropIfExists('doctor_available_days');
     }
 };
--- database/migrations/2023_05_21_213237_create_doctors_available_slots_table.php
+++ database/migrations/2023_05_21_213237_create_doctors_available_slots_table.php
@@ -8,10 +8,8 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('doctor_available_slots', function (Blueprint $table) {
             $table->id();
@@ -25,11 +23,9 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('expert_available_slots');
+        Schema::dropIfExists('doctor_available_slots');
     }
 };
